ac1fabc19e9cbc1104265e324920a18659cd5700
AmeliaBeep <ameliafjjames@gmail.com>
2025-11-12 16:48:07 +0000

    Add initial create post logic

---
+++ homefeed/forms.py
@@ -0,0 +1,15 @@
+from cloudinary.forms import CloudinaryFileField
+from django import forms
+
+from .models import Comment, Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('caption', 'text', 'image')
+
+    image = CloudinaryFileField(
+        options={"folder": "homefeed/",
+                 "crop": "limit", "width": 600, "height": 600, }
+        )
--- homefeed/urls.py
+++ homefeed/urls.py
@@ -4,4 +4,5 @@ from . import views  # import views.py from the current directory
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='homefeed'),
+    path('create-post', views.create_post, name='create_post'),
 ]
