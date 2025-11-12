1800f0e88653d3e48aa6e8132c3d89dc9d33e507
AmeliaBeep <ameliafjjames@gmail.com>
2025-11-12 20:01:28 +0000

    Tweak comments

--- homefeed/views.py
+++ homefeed/views.py
@@ -24,9 +24,8 @@ class PostList(generic.ListView):
 @login_required
 def create_post(request):
 
-    # Handle if POST request
+    # Handle if POST request.
     if request.method == "POST":
-
         handle_post_update(request, operation="Create")
         return HttpResponseRedirect(reverse('homefeed'))
 
@@ -46,7 +45,7 @@ def edit_post(request, post_id):
 
     post = get_object_or_404(Post, pk=post_id)
 
-    # Redirect unauthorised users back to home page
+    # Redirect unauthorised users back to home page.
     if request.user != post.user:
         messages.add_message(
             request, messages.ERROR,
@@ -54,7 +53,7 @@ def edit_post(request, post_id):
         )
         return HttpResponseRedirect(reverse('homefeed'))
 
-    # Handle if POST request
+    # Handle if POST request.
     if request.method == "POST":
         handle_post_update(request, operation="Update", instance=post)
         return HttpResponseRedirect(reverse('homefeed'))
@@ -62,7 +61,11 @@ def edit_post(request, post_id):
 
     # Render page if GET request.
     else:
-        post_form = PostForm(initial={'caption':post.caption, 'text': post.text, 'image':post.image, })
+        post_form = PostForm(
+            initial={'caption':post.caption,
+                     'text': post.text,
+                     'image':post.image, })
+
         return render(
             request,
             "homefeed/edit_user_post.html",
@@ -92,7 +95,7 @@ def handle_post_update(request, operation, instance=None):
     The PostForm object is constructed, including passing it any existing
     post to update if it was called by the update function.
 
-    Images are checked to ensure they exist and to validate their content_type 
+    Images are checked to ensure they exist and to validate their content_type
     is one of the accepted values. If not, then an error is added to the
     post_form instance to make it fail its is_valid() method.
 
@@ -107,10 +110,12 @@ def handle_post_update(request, operation, instance=None):
 
     post_form = PostForm(request.POST, request.FILES, instance=instance)
 
+    # Get image submitted or make form invalid.
     try:
         image = request.FILES.get('image')
         content_type = image.content_type
         valid_content = ["image/jpeg", "image/png", "image/svg+xml"]
+
         if content_type not in valid_content:
             messages.add_message(
                 request, messages.ERROR,
@@ -120,10 +125,12 @@ def handle_post_update(request, operation, instance=None):
             raise ValueError(
                 f'File content_type not in {valid_content}. '
                 + f'Instead it was {content_type}.')
+
+    # AttributeError is automatically raised if no image can be found.
     except (AttributeError, ValueError) as error:
         post_form.add_error('image', error)
 
-    # Save valid post or redirect on error.
+    # Save valid post or add error to messages.
     if post_form.is_valid():
         post = post_form.save(commit=False)
         post.user = request.user
